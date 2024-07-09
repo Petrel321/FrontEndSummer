7fb81a3cf009d21b3f15fe569b510b151d6a2ea2
Ivan Sannikov <i.sannikov@innopolis.university>
2024-07-10 00:26:59 +0300

    Added ESLint and Prettier

---
+++ .eslintrc.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
